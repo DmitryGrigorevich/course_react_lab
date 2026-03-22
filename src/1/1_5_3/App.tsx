9725632b7cba9fa465ac4b6786f6b49c6b200675
DmitryGrigorevich <mitya_107@icloud.com>
2026-03-23 00:07:42 +0300

    1_5_3

--- src/1/1_5_3/App.tsx
+++ src/1/1_5_3/App.tsx
@@ -1,29 +1,56 @@
 // 1_5_3 Passing JSX in a children prop
 /*
-  Извлеките компонент Card из приведенной ниже разметки и используйте параметр children для передачи ему различных JSX.
+  Извлеките компонент Card из приведенной ниже разметки и 
+	используйте параметр children для передачи ему различных JSX.
 */
+
+type AvatarProps = {
+	src:string,
+	alt: string,
+	size: number
+}
+
+function Avatar({src, alt, size}: AvatarProps) {
+	return (
+		<img
+		className="avatar"
+		src={src}
+		alt={alt}
+		width={size}
+		height={size}
+		/>
+	)
+}
+
+function Card({ children }) {
+  return (
+    <>
+      <div className="card">
+        <div className="card-content">{children}</div>
+      </div>
+    </>
+  );
+}
 
 export default function Profile() {
   return (
     <div>
-      <div className="card">
-        <div className="card-content">
-          <h1>Photo</h1>
-          <img
-            className="avatar"
-            src="OKS67lhm.jpg"
-            alt="Aklilu Lemma"
-            width={70}
-            height={70}
-          />
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-content">
-          <h1>About</h1>
-          <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-        </div>
-      </div>
+      <Card>
+        <h1>Photo</h1>
+				<Avatar
+					src="OKS67lhm.jpg"
+					alt="Aklilu Lemma"
+					size={70}
+				/>
+      </Card>
+
+      <Card>
+        <h1>About</h1>
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a
+          natural treatment to schistosomiasis.
+        </p>
+      </Card>
     </div>
   );
 }
