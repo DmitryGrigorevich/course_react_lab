7713def1404a24e7d16ab0597c618da9b09ccf92
DmitryGrigorevich <mitya_107@icloud.com>
2026-03-22 15:07:04 +0300

    1_5_1

--- src/1/1_5_1/App.tsx
+++ src/1/1_5_1/App.tsx
@@ -1,63 +1,82 @@
 // 1_5_1 Extract a component
 /* 
-  Компонент Gallery содержит очень похожую разметку для двух профилей. Извлеките из него компонент Profile, чтобы уменьшить дублирование кода. Для этого нужно будет определить props для компонента Profile.
+  Компонент Gallery содержит очень похожую разметку для двух профилей. 
+	Извлеките из него компонент Profile, чтобы уменьшить дублирование кода. 
+	Для этого нужно будет определить props для компонента Profile.
 */
 
 import { getImageUrl } from "./util";
 
+type ProfileType = {
+	src: string,
+	name: string,
+	width: number,
+	height: number,
+	profession: string,
+	awardsCount: number,
+	awardsText: string,
+	discovered: string
+}
+
+function Profile({
+		src, name, 
+		width, height, profession, 
+		awardsCount, awardsText, discovered}: 
+		ProfileType) {
+	return ( 
+		<>
+		<section className="profile">
+			<h2>{name}</h2>
+			<img 
+				className="avatar"
+				src={src}
+				alt={name}
+				width={width}
+				height={height}
+			/>
+			<ul>
+				<li>
+					<b>Profession: </b> 
+					{profession}
+				</li>
+				<li>
+					<b>Awards: {awardsCount} </b> 
+					({awardsText})
+				</li>
+				<li>
+					<b>Discovered: </b>
+					{discovered}
+				</li>
+			</ul>
+		</section>
+	</>	
+	);
+}
 
 export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('Maria')}
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b> 
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (chemical element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('KatsukoSaruhashi')}
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b> 
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+			<Profile
+				src={getImageUrl('Maria')}
+				name="Maria Skłodowska-Curie"
+				width={70}
+				height={70}
+				profession="physicist and chemist"
+				awardsCount={4}
+				awardsText="(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)"
+				discovered="polonium (chemical element)"
+			/>
+			<Profile
+				name = "Katsuko Saruhashi"
+				src={getImageUrl('KatsukoSaruhashi')}
+				width={70}
+				height={70}
+				profession="geochemist"
+				awardsCount={2}
+				awardsText="(Miyake Prize for geochemistry, Tanaka Prize)"
+				discovered="a method for measuring carbon dioxide in seawater"
+			/>
     </div>
   );
 }
