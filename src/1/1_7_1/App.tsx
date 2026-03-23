0157d8a983a19895afc8988be19338a15e2099b8
DmitryGrigorevich <mitya_107@icloud.com>
2026-03-23 13:32:50 +0300

    1_7_1

--- src/1/1_7_1/App.tsx
+++ src/1/1_7_1/App.tsx
@@ -1,8 +1,8 @@
 // 1_7_1 Splitting a list in two 
 /*
   В этом примере показан список всех людей.
-
-  Измените его, чтобы последовательно вывести два отдельных списка: Химики и Все остальные. Как и ранее, вы можете определить, является ли человек химиком, проверив, что person.profession === 'chemist'.
+  Измените его, чтобы последовательно вывести два отдельных списка: Химики и Все остальные. 
+	Как и ранее, вы можете определить, является ли человек химиком, проверив, что person.profession === 'chemist'.
 */
 
 import { people } from './data.js';
@@ -17,23 +17,50 @@ export type Person = {
 }
 
 export default function List() {
-    const listItems = people.map((person) => (
-        <li key={person.id}>
-            <img
-                src={getImageUrl(person)}
-                alt={person.name}
-            />
-            <p>
-                <b>{person.name}:</b>
-                {' ' + person.profession + ' '}
-                known for {person.accomplishment}
-            </p>
-        </li>
-    ));
+	const chemist = people.filter((person) => person.profession === 'chemist');
+	const otherProfession = people.filter((person) => person.profession != 'chemist');
+
+
+	const listChemist = chemist.map((person) => (
+		<li key={person.id}>
+			<img
+				src={getImageUrl(person)}
+				alt={person.name}
+			/>
+			<p>
+				<b>{person.name}:</b>
+				{' ' + person.profession + ' '}
+				known for {person.accomplishment}
+			</p>
+		</li>
+	));
+
+	const listOther = otherProfession.map((person) => (
+		<li key={person.id}>
+			<img
+				src={getImageUrl(person)}
+				alt={person.name}
+			/>
+			<p>
+				<b>{person.name}:</b>
+				{' ' + person.profession + ' '}
+				known for {person.accomplishment}
+			</p>
+		</li>
+	));
+
+
     return (
         <article>
-            <h1>Scientists</h1>
-            <ul>{listItems}</ul>
+						<h1>Scientists</h1>
+            <h2>Chemists</h2>
+            <ul>
+							{listChemist}
+						</ul>
+						<h2>Everyone Else</h2>
+            <ul>
+							{listOther}
+						</ul>
         </article>
     );
 }
