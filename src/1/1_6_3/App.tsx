8b732793eef2e3de3476b85828a56ed06fb725d5
DmitryGrigorevich <mitya_107@icloud.com>
2026-03-23 12:53:05 +0300

    1_6_3

--- src/1/1_6_3/App.tsx
+++ src/1/1_6_3/App.tsx
@@ -1,26 +1,50 @@
-// 1_6_3 Refactor a series of ? : to if and variables 
+// 1_6_3 Refactor a series of  : to if and variables 
 /*
-Этот компонент Drink использует серию ? : условий для отображения различной информации в зависимости от того, является ли name пропс "чай" или "кофе". Проблема в том, что информация о каждом напитке распределена по нескольким условиям. Переработайте этот код, чтобы использовать один оператор if вместо трех ? : условий.
+Этот компонент Drink использует серию  : условий для отображения различной информации 
+в зависимости от того, является ли name пропс "чай" или "кофе". 
+Проблема в том, что информация о каждом напитке распределена по нескольким условиям. 
+Переработайте этот код, чтобы использовать один оператор if вместо трех  : условий.
 */
 
+
+type DrinkData = {
+	typeData: string,
+	size: string,
+	year: string
+}
+
 function Drink({ name }: { name: string }) {
+
+	let dataInput: DrinkData;
+
+	if (name === 'tea') {
+		dataInput = {
+			typeData: 'leaf',
+			size: '15–70 mg/cup', 
+			year: '4,000+ years'
+		}
+	}
+	else {
+		dataInput = {
+			typeData: 'bean',
+			size: '80–185 mg/cup', 
+			year: '1,000+ years'
+		}
+	}
+
   return (
       <section>
           <h1>{name}</h1>
           <dl>
               <dt>Part of plant</dt>
-              <dd>{name === 'tea' ? 'leaf' : 'bean'}</dd>
+              <dd>{dataInput.typeData}</dd>
               <dt>Caffeine content</dt>
               <dd>
-                  {name === 'tea'
-                      ? '15–70 mg/cup'
-                      : '80–185 mg/cup'}
+                  {dataInput.size}
               </dd>
               <dt>Age</dt>
               <dd>
-                  {name === 'tea'
-                      ? '4,000+ years'
-                      : '1,000+ years'}
+                  {dataInput.year}
               </dd>
           </dl>
       </section>
