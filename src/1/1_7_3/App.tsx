d1a1a14f0c82e954a3d364f825b64ba1cb0ce1a3
DmitryGrigorevich <mitya_107@icloud.com>
2026-03-23 16:50:41 +0300

    1_7_3

--- src/1/1_7_3/App.tsx
+++ src/1/1_7_3/App.tsx
@@ -1,23 +1,43 @@
 // 1_7_3 Extracting a list item component
 /*
-  Этот компонент RecipeList содержит два вложенных вызова map. Чтобы упростить его, извлеките из него компонент Recipe, который будет принимать пропсы id, name и ingredients. Где вы разместите внешний key и почему?.
+  Этот компонент RecipeList содержит два вложенных вызова map. 
+	Чтобы упростить его, извлеките из него компонент Recipe, 
+	который будет принимать пропсы id, name и ingredients. 
+	Где вы разместите внешний key и почему?.
 */
 
 import { recipes } from "./data";
+
+type recipeForm = {
+	id: string,
+	name: string,
+	ingredients: string[]
+}
+
+export function Recipe({id, name, ingredients}: recipeForm) {
+	return (
+		<div>
+			<h2>{name}</h2>
+			<ul>
+				{ingredients.map((ingredient: string) => (
+					<li key={ingredient}>{ingredient}</li>
+				))}
+			</ul>
+		</div>
+	);
+}
 
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map((ingredient) => (
-              <li key={ingredient}>{ingredient}</li>
-            ))}
-          </ul>
-        </div>
+				<Recipe 
+					key={recipe.id} 
+					id = {recipe.id}
+					name = {recipe.name}
+					ingredients={recipe.ingredients}
+					/>
       ))}
     </div>
   );
