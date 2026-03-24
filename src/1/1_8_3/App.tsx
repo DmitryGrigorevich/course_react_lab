6a3ec73d7c112ee18a57cae53905851a545cad3e
DmitryGrigorevich <mitya_107@icloud.com>
2026-03-24 23:16:10 +0300

    1_8_3

--- src/1/1_8_3/App.tsx
+++ src/1/1_8_3/App.tsx
@@ -1,8 +1,14 @@
 // 1_8_3 Fix a broken story tray
 /*
-  Генеральный директор вашей компании просит вас добавить "истории" в ваше приложение онлайн-часов, и вы не можете отказать. Вы написали компонент StoryTray, который принимает список stories, за которым следует заполнитель "Create Story".
+  Генеральный директор вашей компании просит вас добавить "истории" 
+	в ваше приложение онлайн-часов, и вы не можете отказать. 
+	Вы написали компонент StoryTray, который принимает список stories, 
+	за которым следует заполнитель "Create Story".
 
-  Вы реализовали заполнитель "Create Story", поместив еще одну фальшивую историю в конец массива stories, который вы получаете в качестве пропса. Но по какой-то причине "Create Story" появляется более одного раза. Исправьте эту проблему.
+  Вы реализовали заполнитель "Create Story", 
+	поместив еще одну фальшивую историю в конец массива stories, 
+	который вы получаете в качестве пропса. 
+	Но по какой-то причине "Create Story" появляется более одного раза. Исправьте эту проблему.
 */
 
 export interface Story {
@@ -11,21 +17,28 @@ export interface Story {
 } 
 
 const stories: Story[] = [];
-export default function Wrapper(){
-  return <StoryTray stories={stories}/>;
-}
 
- function StoryTray({ stories }: { stories: Story[] }) {
-  stories.push({
+
+function StoryTray({ stories }: { stories: Story[] }) {
+
+	const newStories: Story[] = [
+		...stories
+	]
+  newStories.push({
       id: 'create',
       label: 'Create Story',
   });
 
   return (
       <ul>
-          {stories.map((story) => (
+          {newStories.map((story) => (
               <li key={story.id}>{story.label}</li>
           ))}
       </ul>
   );
 }
+
+export default function Wrapper(){
+  return <StoryTray stories={stories}/>;
+}
+
