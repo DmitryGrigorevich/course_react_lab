f851ec4979406f598c477340e1118dbe621cbbac
DmitryGrigorevich <mitya_107@icloud.com>
2026-03-24 15:41:55 +0300

    1_8_2

--- src/1/1_8_2/Profile.tsx
+++ src/1/1_8_2/Profile.tsx
@@ -1,28 +1,26 @@
 import Panel from './Panel.js';
 import { getImageUrl } from './utils.js';
 
-let currentPerson;
 
 export default function Profile({ person }) {
-  currentPerson = person;
   return (
     <Panel>
-      <Header />
-      <Avatar />
+      <Header name={person.name}/>
+      <Avatar person={person}/>
     </Panel>
   )
 }
 
-function Header() {
-  return <h1>{currentPerson.name}</h1>;
+function Header({ name }) {
+  return <h1>{name}</h1>;
 }
 
-function Avatar() {
+function Avatar({ person }) {
   return (
     <img
       className="avatar"
-      src={getImageUrl(currentPerson)}
-      alt={currentPerson.name}
+      src={getImageUrl(person.imageId)}
+      alt={person.name}
       width={50}
       height={50}
     />
