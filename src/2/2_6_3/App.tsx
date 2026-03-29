7e159040ca0811425c3af2cb02ce299c9a285733
DmitryGrigorevich <mitya_107@icloud.com>
2026-03-29 18:08:29 +0300

    2_6_3

--- src/2/2_6_3/App.tsx
+++ src/2/2_6_3/App.tsx
@@ -2,12 +2,16 @@
 /*
     Имеется перетаскиваемый ящик на статичном фоне. Вы можете изменить цвет поля с помощью кнопки select.
 
-    Но есть ошибка. Если сначала переместить ящик, а затем изменить его цвет, фон (который не должен двигаться!) "перепрыгнет" на позицию ящика. Но этого не должно произойти: параметр position у Background установлен в initialPosition, что равно { x: 0, y: 0 }. Почему фон перемещается после изменения цвета?
+    Но есть ошибка. Если сначала переместить ящик, а затем изменить его цвет, 
+		фон (который не должен двигаться!) "перепрыгнет" на позицию ящика. 
+		Но этого не должно произойти: параметр position у Background установлен в initialPosition, 
+		что равно { x: 0, y: 0 }. Почему фон перемещается после изменения цвета?
 
-    На этот раз исправьте мутацию, используя Immer. Для вашего удобства функция useImmer уже импортирована, поэтому вам нужно изменить переменную состояния shape, чтобы использовать ее.
+    На этот раз исправьте мутацию, используя Immer. 
+		Для вашего удобства функция useImmer уже импортирована, 
+		поэтому вам нужно изменить переменную состояния shape, чтобы использовать ее.
 */
 
-import { useState } from 'react';
 import { useImmer } from 'use-immer';
 import Background from './Background';
 import Box from './Box';
@@ -20,21 +24,23 @@ const initialPosition = {
 };
 
 export default function Canvas() {
-    const [shape, setShape] = useState({
+    const [shape, updateShape] = useImmer({
         color: 'orange',
         position: initialPosition,
     });
 
     function handleMove(dx: number, dy: number) {
-        shape.position.x += dx;
-        shape.position.y += dy;
+
+			updateShape((obj) => {
+				obj.position.x += dx
+				obj.position.y += dy
+			})
     }
 
     function handleColorChange(e: any) {
-        setShape({
-            ...shape,
-            color: e.target.value,
-        });
+			updateShape((obj) => {
+				obj.color = e.target.value
+			})
     }
 
     return (
