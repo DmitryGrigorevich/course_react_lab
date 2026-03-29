67b9e340e18fc352141847336649a7e2a169e9e2
DmitryGrigorevich <mitya_107@icloud.com>
2026-03-29 15:54:27 +0300

    2_5_2

--- src/2/2_5_2/processQueue.ts
+++ src/2/2_5_2/processQueue.ts
@@ -1,9 +1,13 @@
 import { Que } from "./App";
 
 export function getFinalState(baseState: number, queue: Que[]) {
-    let finalState = baseState;
+	let finalState = baseState;
+	
+	// TODO: do something with the queue...
+	for (const elem of queue) {
+		if (typeof elem === "number") finalState = elem;
+		else finalState = elem(finalState)
+	}
 
-    // TODO: do something with the queue...
-
-    return finalState;
+	return finalState;
 }
