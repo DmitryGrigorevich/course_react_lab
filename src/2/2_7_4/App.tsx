3b56cc390a158b17f753b50ca7864ce18809976c
DmitryGrigorevich <mitya_107@icloud.com>
2026-03-31 01:32:52 +0300

    2_7_4

--- src/2/2_7_4/App.tsx
+++ src/2/2_7_4/App.tsx
@@ -1,9 +1,10 @@
 // 2_7_4 Fix the mutations using Immer  
 /*
-    В этом примере все обработчики событий в App.js используют мутацию. В результате редактирование и удаление todos не работает. Перепишите handleAddTodo, handleChangeTodo и handleDeleteTodo с помощью Immer
+    В этом примере все обработчики событий в App.js используют мутацию. 
+		В результате редактирование и удаление todos не работает. 
+		Перепишите handleAddTodo, handleChangeTodo и handleDeleteTodo с помощью Immer
 */
 
-import { useState } from 'react';
 import { useImmer } from 'use-immer';
 import AddTodo from './AddTodo';
 import TaskList from './TaskList';
@@ -22,29 +23,37 @@ const initialTodos = [
 ];
 
 export default function TaskApp() {
-    const [todos, setTodos] = useState(initialTodos);
+    const [todos, updateTodos] = useImmer(initialTodos);
 
     function handleAddTodo(title: string) {
-        todos.push({
-            id: nextId++,
-            title: title,
-            done: false,
-        });
+			updateTodos((elem) => {elem.push(
+				{
+					id: nextId++,
+					title: title,
+					done: false,
+				});
+			});
     }
 
     function handleChangeTodo(nextTodo: Todo) {
-        const todo = todos.find(
-            (t) => t.id === nextTodo.id
-        );
-        todo.title = nextTodo.title;
-        todo.done = nextTodo.done;
+			updateTodos((elem) => {
+				const todo = elem.find(
+					(t) => t.id === nextTodo.id
+				);
+				todo.title = nextTodo.title;
+				todo.done = nextTodo.done;
+	
+			});			
     }
 
     function handleDeleteTodo(todoId: number) {
-        const index = todos.findIndex(
-            (t) => t.id === todoId
-        );
-        todos.splice(index, 1);
+			updateTodos((elem) => {
+				const index = elem.findIndex(
+					(t) => t.id === todoId
+				);
+				elem.splice(index, 1);
+			});
+
     }
 
     return (
