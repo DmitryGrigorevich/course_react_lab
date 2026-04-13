dec06fb480706792579f5712c43e566ad442464a
DmitryGrigorevich <mitya_107@icloud.com>
2026-04-14 01:54:46 +0300

    3_1_2

--- src/3/3_1_2/App.tsx
+++ src/3/3_1_2/App.tsx
@@ -1,30 +1,70 @@
-// 3_1_2 Profile editor 
+// 3_1_2 Profile editor
 /*
     В проекте приведена форма, реализованная на чистом JavaScript.
 
-    Эта форма переключается между двумя режимами: в режиме редактирования вы видите вводимые данные, а в режиме просмотра - только результат. Метка кнопки меняется между "Редактировать" и "Сохранить" в зависимости от того, в каком режиме вы находитесь. Когда вы изменяете вводимые данные, приветственное сообщение внизу обновляется в режиме реального времени.
+    Эта форма переключается между двумя режимами: в режиме редактирования вы видите вводимые данные, 
+		а в режиме просмотра - только результат. 
+		Метка кнопки меняется между "Редактировать" и "Сохранить" в зависимости от того, 
+		в каком режиме вы находитесь. 
+		Когда вы изменяете вводимые данные, приветственное сообщение внизу обновляется в режиме реального времени.
 
-    Ваша задача - реализовать это на React. Для вашего удобства разметка уже была преобразована в JSX, но вам нужно будет сделать так, чтобы она показывала и скрывала входы, как это делает оригинал.
+    Ваша задача - реализовать это на React. Для вашего удобства разметка уже была преобразована в JSX, 
+		но вам нужно будет сделать так, чтобы она показывала и скрывала входы, как это делает оригинал.
 
     Убедитесь, что она также обновляет текст внизу!
 */
 
+import { useState } from "react";
 
 export default function EditProfile() {
-    return (
-        <form>
-            <label>
-                First name: <b>Jane</b>
-                <input />
-            </label>
-            <label>
-                Last name: <b>Jacobs</b>
-                <input />
-            </label>
-            <button type="submit">Edit Profile</button>
-            <p>
-                <i>Hello, Jane Jacobs!</i>
-            </p>
-        </form>
-    );
+  const [name, setName] = useState("Jane");
+  const [surname, setSurname] = useState("Jacobs");
+  const [edit, setEdit] = useState(false);
+
+  return (
+    <form onSubmit={(event) => event.preventDefault()}>
+      {!edit ? (
+        <label>
+          First name: <b>{name}</b>
+        </label>
+      ) : (
+        <label>
+          First name:
+          {edit && (
+            <input
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+          )}
+        </label>
+      )}
+
+      {!edit ? (
+        <label>
+          First surname: <b>{surname}</b>
+        </label>
+      ) : (
+        <label>
+          First surname:
+          {edit && (
+            <input
+              value={surname}
+              onChange={(event) => setSurname(event.target.value)}
+            />
+          )}
+        </label>
+      )}
+
+      {!edit ? (
+        <button type="submit" onClick={() => setEdit(true)}>Edit Profile</button>
+      ) : (
+        <button type="submit" onClick={() => setEdit(false)}>Save Profile</button>
+      )}
+      <p>
+        <i>
+          Hello, {name} {surname}!
+        </i>
+      </p>
+    </form>
+  );
 }
