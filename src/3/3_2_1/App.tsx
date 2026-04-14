128b5a880dde2d0ac56113edd73ce4331f986292
DmitryGrigorevich <mitya_107@icloud.com>
2026-04-14 14:50:32 +0300

    3_2_1

--- src/3/3_2_1/App.tsx
+++ src/3/3_2_1/App.tsx
@@ -1,14 +1,17 @@
-// 3_2_1 Fix a component that’s not updating 
+// 3_2_1 Fix a component that’s not updating
 /*
-    Компонент Clock получает два пропса: color и time. Когда вы выбираете другой цвет в поле выбора, компонент Clock получает другой пропс color от своего родительского компонента. Однако по какой-то причине отображаемый цвет не обновляется. Почему? Устраните проблему.
+    Компонент Clock получает два пропса: color и time. 
+		Когда вы выбираете другой цвет в поле выбора, 
+		компонент Clock получает другой пропс color от своего родительского компонента. 
+		Однако по какой-то причине отображаемый цвет не обновляется. Почему? Устраните проблему.
 */
 
-
-import { useState, useEffect } from 'react';
-import Clock from './Clock';
+import { useState, useEffect } from "react";
+import Clock from "./Clock";
 
 function useTime() {
   const [time, setTime] = useState(() => new Date());
+
   useEffect(() => {
     const id = setInterval(() => {
       setTime(new Date());
@@ -20,12 +23,12 @@ function useTime() {
 
 export default function App() {
   const time = useTime();
-  const [color, setColor] = useState('lightcoral');
+  const [color, setColor] = useState("lightcoral");
   return (
     <div>
       <p>
-        Pick a color:{' '}
-        <select value={color} onChange={e => setColor(e.target.value)}>
+        Pick a color:{" "}
+        <select value={color} onChange={(e) => setColor(e.target.value)}>
           <option value="lightcoral">lightcoral</option>
           <option value="midnightblue">midnightblue</option>
           <option value="rebeccapurple">rebeccapurple</option>
