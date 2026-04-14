0c4c85c0f2d7832bf56f63602c7d7da1035754bd
DmitryGrigorevich <mitya_107@icloud.com>
2026-04-14 17:16:31 +0300

    3_2_4

--- src/3/3_2_4/App.tsx
+++ src/3/3_2_4/App.tsx
@@ -1,8 +1,15 @@
 // 3_2_4 Implement multiple selection
 /*
-    В этом примере каждое Letter имеет свойство isSelected и обработчик onToggle, который отмечает его как выбранное. Это работает, но состояние хранится как selectedId (либо null, либо ID), поэтому в каждый момент времени может быть выбрано только одно письмо.
+    В этом примере каждое Letter имеет свойство isSelected и обработчик onToggle, 
+		который отмечает его как выбранное. 
+		Это работает, но состояние хранится как selectedId (либо null, либо ID), 
+		поэтому в каждый момент времени может быть выбрано только одно письмо.
 
-    Измените структуру состояния для поддержки множественного выбора (Как бы вы его структурировали? Подумайте об этом перед написанием кода). Каждый флажок должен стать независимым от других. Щелчок по выбранному письму должен снимать флажок. Наконец, нижний колонтитул должен показывать правильное количество выбранных элементов.
+    Измените структуру состояния для поддержки множественного выбора 
+		(Как бы вы его структурировали? Подумайте об этом перед написанием кода). 
+		Каждый флажок должен стать независимым от других. 
+		Щелчок по выбранному письму должен снимать флажок. 
+		Наконец, нижний колонтитул должен показывать правильное количество выбранных элементов.
 */
 
 
@@ -11,14 +18,21 @@ import { letters } from './data.js';
 import Letter from './Letter.js';
 
 export default function MailClient() {
-  const [selectedId, setSelectedId] = useState<number | null>(null);
-
   // TODO: allow multiple selection
-  const selectedCount = 1;
+	const [selectedIds, setSelectedIds] = useState<number[]>([]);
+
+  const selectedCount = selectedIds.length;
 
   function handleToggle(toggledId: number) {
-    // TODO: allow multiple selection
-    setSelectedId(toggledId);
+		// TODO: allow multiple selection
+		if (selectedIds.includes(toggledId)) {
+			setSelectedIds(selectedIds.filter((id) => id!== toggledId))
+		}
+		else {
+			setSelectedIds([...selectedIds, toggledId])
+		}
+    
+    //setSelectedId(toggledId);
   }
 
   return (
@@ -31,7 +45,7 @@ export default function MailClient() {
             letter={letter}
             isSelected={
               // TODO: allow multiple selection
-              letter.id === selectedId
+							selectedIds.includes(letter.id)
             }
             onToggle={handleToggle}
           />
