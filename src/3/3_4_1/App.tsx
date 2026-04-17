4a7c4b79d483108173d5242d53c3fb13dad8aa9b
DmitryGrigorevich <mitya_107@icloud.com>
2026-04-17 14:32:48 +0300

    3_4_1

--- src/3/3_4_1/App.tsx
+++ src/3/3_4_1/App.tsx
@@ -1,40 +1,35 @@
-// 3_4_1 Fix disappearing input text 
+// 3_4_1 Fix disappearing input text
 /*
-  Этот пример показывает сообщение при нажатии на кнопку. Однако при нажатии кнопки также происходит случайный сброс ввода. Почему так происходит? Исправьте, чтобы нажатие кнопки не сбрасывало вводимый текст.
+  Этот пример показывает сообщение при нажатии на кнопку. 
+	Однако при нажатии кнопки также происходит случайный сброс ввода. 
+	Почему так происходит? Исправьте, чтобы нажатие кнопки не сбрасывало вводимый текст.
 */
 
-import { useState } from 'react';
+import { useState } from "react";
+
+function Form() {
+  const [text, setText] = useState("");
+  return <textarea value={text} onChange={(e) => setText(e.target.value)} />;
+}
 
 export default function App() {
   const [showHint, setShowHint] = useState(false);
-  if (showHint) {
-    return (
-      <div>
-        <p><i>Hint: Your favorite city?</i></p>
-        <Form />
-        <button onClick={() => {
-          setShowHint(false);
-        }}>Hide hint</button>
-      </div>
-    );
-  }
+
+  const handleShow = () => {
+    setShowHint(!showHint);
+  };
+
   return (
     <div>
+      {showHint && (
+        <p>
+          <i>Hint: Your favorite city?</i>
+        </p>
+      )}
       <Form />
-      <button onClick={() => {
-        setShowHint(true);
-      }}>Show hint</button>
+      <button onClick={handleShow}>
+        {showHint ? "Hide hint" : "Show hint"}
+      </button>
     </div>
   );
 }
-
-function Form() {
-  const [text, setText] = useState('');
-  return (
-    <textarea
-      value={text}
-      onChange={e => setText(e.target.value)}
-    />
-  );
-}
-
