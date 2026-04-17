06a45a3c177570ed9cc443d9468a4aed8d82b75c
DmitryGrigorevich <mitya_107@icloud.com>
2026-04-17 13:07:01 +0300

    3_3_1

--- src/3/3_3_1/App.tsx
+++ src/3/3_3_1/App.tsx
@@ -1,33 +1,37 @@
 // 3_3_1 Synced inputs 
 /*
-  Эти два входа являются независимыми. Сделайте их синхронизированными: редактирование одного входа должно обновить другой вход с тем же текстом, и наоборот.
+  Эти два входа являются независимыми. 
+	Сделайте их синхронизированными: редактирование одного входа должно обновить другой вход с тем же текстом, 
+	и наоборот.
 */
-
 import { useState } from 'react';
 
+type InputProps = {
+	label: string,
+	value: string,
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
 export default function SyncedInputs() {
+
+	const [text, setText] = useState('')
+
   return (
     <>
-      <Input label="First input" />
-      <Input label="Second input" />
+      <Input label="First input" value={text} onChange={(e) => setText(e.target.value)}/>
+      <Input label="Second input" value={text} onChange={(e) => setText(e.target.value)}/>
     </>
   );
 }
 
-function Input({ label }: { label: string }) {
-  const [text, setText] = useState('');
-
-  function handleChange(e) {
-    setText(e.target.value);
-  }
-
+function Input({ label, value, onChange }: InputProps) {
   return (
     <label>
       {label}
       {' '}
       <input
-        value={text}
-        onChange={handleChange}
+        value={value}
+        onChange={onChange}
       />
     </label>
   );
