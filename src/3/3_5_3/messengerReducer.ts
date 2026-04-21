6886503233928f4688be14aef161ce95f3f7ac41
DmitryGrigorevich <mitya_107@icloud.com>
2026-04-21 23:55:14 +0300

    3_5_3

--- src/3/3_5_3/messengerReducer.ts
+++ src/3/3_5_3/messengerReducer.ts
@@ -1,6 +1,6 @@
 export type State = {
     selectedId: number;
-    message: string;
+    messages: Record <number, string>;
 };
 
 export type Action = {
@@ -15,7 +15,11 @@ export type Action = {
 
 export const initialState = {
     selectedId: 0,
-    message: 'Hello',
+    messages: {
+			0: 'Hello',
+			1: '', 
+			2: ''
+		}
 };
 
 export function messengerReducer(
@@ -27,19 +31,24 @@ export function messengerReducer(
             return {
                 ...state,
                 selectedId: action.contactId,
-                message: '',
             };
         }
         case 'edited_message': {
             return {
                 ...state,
-                message: action.message,
+                messages: {
+									...state.messages,
+									[state.selectedId]: action.message
+								}
             };
         }
         case 'sent_message': {
             return {
               ...state,
-              message: '',
+              messages: {
+								...state.messages,
+								[state.selectedId]: ''
+							}
             };
           }        
         default: {
