93b5c6cd2433fbd0a43e4907df55bfecd837fa46
DmitryGrigorevich <mitya_107@icloud.com>
2026-04-28 14:59:28 +0300

    3_6_1

--- src/3/3_6_1/App.tsx
+++ src/3/3_6_1/App.tsx
@@ -1,77 +1,73 @@
 // 3_6_1  Replace prop drilling with context
 /*
-  В этом примере переключение флажка изменяет параметр imageSize, передаваемый каждому <PlaceImage>. Состояние флажка хранится в компоненте верхнего уровня App, но каждый <PlaceImage> должен знать об этом.
+  В этом примере переключение флажка изменяет параметр imageSize, 
+	передаваемый каждому <PlaceImage>. 
+	Состояние флажка хранится в компоненте верхнего уровня App, но каждый <PlaceImage> должен знать об этом.
 
-  В настоящее время App передает imageSize в List, который передает его в каждое Place, которое передает его в PlaceImage. Удалите пропс imageSize, и вместо этого передавайте его из компонента App непосредственно в PlaceImage.
-
+  В настоящее время App передает imageSize в List, 
+	который передает его в каждое Place, которое передает его в PlaceImage. 
+	Удалите пропс imageSize, и вместо этого передавайте его из компонента App непосредственно в PlaceImage.
   Вы можете объявить контекст в файле Context.js.
 */
 
-import { useState } from 'react';
-import { places, PlaceType } from './data';
-import { getImageUrl } from './utils';
+import { useState, useContext } from "react";
+import { places, PlaceType } from "./data";
+import { getImageUrl } from "./utils";
+import { ImageSizeContext } from "./Context";
 
-export default function App() {
-  const [isLarge, setIsLarge] = useState(false);
-  const imageSize = isLarge ? 150 : 100;
-  return (
-    <>
-      <label>
-        <input
-          type="checkbox"
-          checked={isLarge}
-          onChange={e => {
-            setIsLarge(e.target.checked);
-          }}
-        />
-        Use large images
-      </label>
-      <hr />
-      <List imageSize={imageSize} />
-    </>
-  )
-}
-
-function List({ imageSize }: { imageSize: number }) {
-  const listItems = places.map(place =>
+function List() {
+  const listItems = places.map((place) => (
     <li key={place.id}>
-      <Place
-        place={place}
-        imageSize={imageSize}
-      />
+      <Place place={place} />
     </li>
-  );
+  ));
   return <ul>{listItems}</ul>;
 }
 
-function Place(
-  { place, imageSize }: 
-  { place: PlaceType, imageSize: number }
-) {
+function Place({ place }: { place: PlaceType }) {
   return (
     <>
-      <PlaceImage
-        place={place}
-        imageSize={imageSize}
-      />
+      <PlaceImage place={place} />
       <p>
         <b>{place.name}</b>
-        {': ' + place.description}
+        {": " + place.description}
       </p>
     </>
   );
 }
 
-function PlaceImage(
-  { place, imageSize }:
-    { place: PlaceType, imageSize: number }
-) {
+export function PlaceImage({ place }: { place: PlaceType }) {
+	const size = useContext(ImageSizeContext)
   return (
     <img
       src={getImageUrl(place)}
       alt={place.name}
-      width={imageSize}
-      height={imageSize}
+      width={size}
+      height={size}
     />
+  );
+}
+
+export default function App() {
+  const [isLarge, setIsLarge] = useState(false);
+
+  const imageSize = isLarge ? 150 : 100;
+  return (
+    <>
+      <ImageSizeContext.Provider value={imageSize}>
+        <label>
+          <input
+            type="checkbox"
+            checked={isLarge}
+            onChange={(e) => {
+              setIsLarge(e.target.checked);
+            }}
+          />
+          Use large images
+        </label>
+        <hr />
+        <List />
+      </ImageSizeContext.Provider>
+    </>
   );
 }
