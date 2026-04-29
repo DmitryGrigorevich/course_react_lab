8fa51897b81deecabf71cc637ae8aaf1ae9f83b1
DmitryGrigorevich <mitya_107@icloud.com>
2026-04-29 20:01:20 +0300

    4_2_2

--- src/4/4_2_2/App.tsx
+++ src/4/4_2_2/App.tsx
@@ -1,15 +1,25 @@
 // 4_2_2 Focus the search field
 /*
-  Сделайте так, чтобы нажатие на кнопку "Поиск" наводило фокус на поле.
+  Сделайте так, чтобы нажатие на кнопку "Поиск" 
+	наводило фокус на поле.
 */
+import {useRef} from "react";
 
 export default function Page() {
+
+	const tmpRef = useRef(null);
+
   return (
     <>
       <nav>
-        <button>Search</button>
+        <button
+					onClick={() => {tmpRef.current.focus()}}
+				>
+					Search
+				</button>
       </nav>
       <input
+				ref={tmpRef}
         placeholder="Looking for something?"
       />
     </>
