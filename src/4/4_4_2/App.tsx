3f4080bd3d97c0cadccde325499db03abfaab60e
DmitryGrigorevich <mitya_107@icloud.com>
2026-05-20 14:21:21 +0300

    4_4_2

--- src/4/4_4_2/App.tsx
+++ src/4/4_4_2/App.tsx
@@ -1,22 +1,25 @@
 // 4_4_2 Cache a calculation without Effects
 /*
-  В этом примере фильтрация тодосов была вынесена в отдельную функцию под названием getVisibleTodos(). Эта функция содержит внутри себя вызов console.log(), который поможет вам заметить, когда она вызывается. Установите флажок "Показывать только активные тодосы" и обратите внимание, что это вызывает повторный запуск getVisibleTodos(). Это ожидаемо, поскольку видимые тодосы меняются, когда вы переключаете, какие из них показывать.
+  В этом примере фильтрация тодосов была вынесена в отдельную функцию под названием getVisibleTodos(). 
+	Эта функция содержит внутри себя вызов console.log(), который поможет вам заметить, когда она вызывается. 
+	Установите флажок "Показывать только активные тодосы" и обратите внимание, 
+	что это вызывает повторный запуск getVisibleTodos(). 
+	Это ожидаемо, поскольку видимые тодосы меняются, когда вы переключаете, какие из них показывать.
 
-  Ваша задача - удалить эффект, который пересчитывает список visibleTodos в компоненте TodoList. Однако, вам нужно убедиться, что getVisibleTodos() не повторно запускается (и поэтому не печатает никаких логов), когда вы вводите данные в input.
+  Ваша задача - удалить эффект, который пересчитывает список visibleTodos в компоненте TodoList. 
+	Однако, вам нужно убедиться, что getVisibleTodos() не повторно запускается 
+	(и поэтому не печатает никаких логов), когда вы вводите данные в input.
 */
 
-import { useState, useEffect } from "react";
+import { useState, useMemo } from "react";
 import { initialTodos, createTodo, getVisibleTodos, Todo } from "./todos.js";
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
   const [showActive, setShowActive] = useState(false);
   const [text, setText] = useState("");
-  const [visibleTodos, setVisibleTodos] = useState<Todo[]>([]);
+  const visibleTodos = useMemo(() => getVisibleTodos(todos, showActive), [todos, showActive]);
 
-  useEffect(() => {
-    setVisibleTodos(getVisibleTodos(todos, showActive));
-  }, [todos, showActive]);
 
   function handleAddClick() {
     setText("");
