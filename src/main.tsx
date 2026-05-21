2eacf4101df1d64cb1b7b027be3acffc7142b492
DmitryGrigorevich <mitya_107@icloud.com>
2026-05-21 20:00:02 +0300

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './4/4_6_1/style.css'
-import App from './4/4_6_1/App.tsx'
+import './4/4_8_5/style.css'
+import App from "./4/4_8_5/App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
